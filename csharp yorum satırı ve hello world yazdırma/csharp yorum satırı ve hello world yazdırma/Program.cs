286fe03656ee016efb47fc93a2dfe968e3cb8f19
Okan Çelebi <okancelebi06@gmail.com>
2023-01-03 18:15:33 +0300

    Add files via upload

---
+++ csharp yorum satırı ve hello world yazdırma/csharp yorum satırı ve hello world yazdırma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_yorum_satırı_ve_hello_world_yazdırma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             //Slaş - Yıldız yaparsak hepsi yorum satırı olur.
+             */
+            Console.WriteLine("Hello World.");
+            Console.WriteLine("Okan Çelebi");
+            Console.Write("Selam,");
+            Console.Write(" Merhaba.");
+            //Console.Write(WriteLine alt satıra atar. Write ise yan yana yazar.)
+            Console.ReadLine();
+        }
+    }
+}
